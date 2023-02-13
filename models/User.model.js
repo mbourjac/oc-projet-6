e5abe0f6439c0894c90e6994bd110993537f421f
mbourjac2 <michael.bourjac@gmail.com>
2023-02-13 19:09:22 +0100

    Create user model

---
+++ models/User.model.js
@@ -0,0 +1,35 @@
+const mangoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+userSchema.pre('save', async function () {
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+	return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.createJWT = function () {
+	return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+		expiresIn: '24h',
+	});
+};
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
