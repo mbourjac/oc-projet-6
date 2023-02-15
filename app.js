c56a88441b308a4602d57aa29fefed196f1d3227
mbourjac2 <michael.bourjac@gmail.com>
2023-02-15 19:48:34 +0100

    Fix missing slash on auth route

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const authRouter = require('./routes/auth.routes');
 app.use(cors);
 app.use(express.json());
 
-app.use('api/auth', authRouter);
+app.use('/api/auth', authRouter);
 
 module.exports = app;
