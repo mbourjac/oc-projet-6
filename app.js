2cfec08d03f3c6929b12e8a4deab39feb0cbfc17
mbourjac2 <michael.bourjac@gmail.com>
2023-02-15 19:17:33 +0100

    Implement auth router on app

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 
 const cors = require('./middleware/cors');
+const authRouter = require('./routes/auth.routes');
 
 app.use(cors);
 app.use(express.json());
+
+app.use('api/auth', authRouter);
 
 module.exports = app;
