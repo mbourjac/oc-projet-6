40d72559542a106475f86ee1ee18cf7e513723b5
mbourjac2 <michael.bourjac@gmail.com>
2023-02-17 16:19:22 +0100

    Create authorization middleware

---
+++ middleware/authorization.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { Unauthorized } = require('../errors');
+
+const authorizeUser = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader || !authHeader.startsWith('Bearer')) {
+		return next(new Unauthorized('Invalid authorization header'));
+	}
+
+	const token = authHeader.split(' ')[1];
+
+	try {
+		const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.user = { userId: payload.userId };
+		next();
+	} catch (error) {
+		res.status(401).json({ error });
+	}
+};
+
+module.exports = authorizeUser;
