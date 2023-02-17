40be129877bef142eb7ceaa96c1d1f3e671ed345
mbourjac2 <michael.bourjac@gmail.com>
2023-02-17 16:23:20 +0100

    Add controllers to auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { signup, login } = require('../controllers/auth.controllers');
 
-router.post('/signup');
-router.post('/login');
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
