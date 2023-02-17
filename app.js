9b3a639602bf9022bdf1aee93537692ad4f461dc
mbourjac2 <michael.bourjac@gmail.com>
2023-02-17 16:25:50 +0100

    Use error handler middleware on app

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 
 const cors = require('./middleware/cors');
+const errorHandler = require('./middleware/error-handler');
+
 const authRouter = require('./routes/auth.routes');
 
 app.use(cors);
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
