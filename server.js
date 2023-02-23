61fa5aace3d5bb738a2b72bcc73af8a2c130b41b
mbourjac2 <michael.bourjac@gmail.com>
2023-02-23 09:07:09 +0100

    Rename db folder to database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
-const connectDB = require('./db/connect');
+const connectDB = require('./database/connect');
 const port = process.env.PORT || 5000;
 
 const start = async () => {
