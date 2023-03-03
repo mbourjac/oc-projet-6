2391c571f17a3f9cb3e10a9c23509be249230394
mbourjac2 <michael.bourjac@gmail.com>
2023-03-03 18:20:27 +0100

    Use cors package on app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const cors = require('./middleware/cors');
+const cors = require('cors');
 const errorHandler = require('./middleware/error-handler');
 
 const authRouter = require('./users/auth.routes');
