16ecaa72f4cbcddb5c4679553b1dd0464af77b9d
mbourjac2 <michael.bourjac@gmail.com>
2023-03-05 09:03:31 +0100

    Add validation middlewares to auth routes

--- users/auth.routes.js
+++ users/auth.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+const { validateSignup, validateLogin } = require('./auth.validation');
+const checkValidation = require('../middleware/check-validation');
 const { signup, login } = require('./auth.controllers');
 
-router.post('/signup', signup);
-router.post('/login', login);
+router.post('/signup', validateSignup, checkValidation, signup);
+router.post('/login', validateLogin, checkValidation, login);
 
 module.exports = router;
