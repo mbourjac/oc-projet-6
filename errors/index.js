838eea86ffce958f73a55f08b0e28b776f143333
mbourjac2 <michael.bourjac@gmail.com>
2023-03-05 08:52:19 +0100

    Add NotFound class to errors/index.js

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,6 @@
 const HttpError = require('./http-error');
 const BadRequest = require('./bad-request');
 const Unauthorized = require('./unauthorized');
+const NotFound = require('./not-found');
 
-module.exports = { HttpError, BadRequest, Unauthorized };
+module.exports = { HttpError, BadRequest, Unauthorized, NotFound };
