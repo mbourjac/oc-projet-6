eea29c69a060b3f4c6c6fbc14a32d70ac47b4172
mbourjac2 <michael.bourjac@gmail.com>
2023-03-11 11:27:09 +0100

    Update imports and middlewares on auth routes

--- users/auth.routes.js
+++ users/auth.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { validateSignup, validateLogin } = require('./auth.validation');
-const checkValidation = require('../middleware/check-validation');
+const {
+	validateSignupData,
+	validateLoginData,
+	validationCheck,
+} = require('./auth.validation');
 const { signup, login } = require('./auth.controllers');
 
-router.post('/signup', validateSignup, checkValidation, signup);
-router.post('/login', validateLogin, checkValidation, login);
+router.post('/signup', validateSignupData, validationCheck, signup);
+router.post('/login', validateLoginData, validationCheck, login);
 
 module.exports = router;
