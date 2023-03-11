b0aafc67e771b16d926515b753e38397ce1ecd6f
mbourjac2 <michael.bourjac@gmail.com>
2023-03-11 11:38:01 +0100

    Create multer index.js

---
+++ multer/index.js
@@ -0,0 +1,6 @@
+const multerSetup = require('./multer-setup');
+const fileCheck = require('./file-check');
+const dataCheck = require('./data-check');
+const dataSetup = require('./data-setup');
+
+module.exports = { multerSetup, fileCheck, dataCheck, dataSetup };
