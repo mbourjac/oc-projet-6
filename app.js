20ce7b3ab74035a720a20263f007634920a74a48
mbourjac2 <michael.bourjac@gmail.com>
2023-03-18 08:58:29 +0100

    Serve static files from the images directory to the /images endpoint

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const authRouter = require('./users/auth.routes');
 
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', authRouter);
 
