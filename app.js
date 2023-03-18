ac000f60b000ab579528b00b9546056e9e2ee103
mbourjac2 <michael.bourjac@gmail.com>
2023-03-18 08:54:40 +0100

    Add path module to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
+const path = require('path');
 const errorHandler = require('./middleware/error-handler');
 
 const authRouter = require('./users/auth.routes');
