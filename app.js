734f62ff2e0589046aa1cb9684d7526404233067
mbourjac2 <michael.bourjac@gmail.com>
2023-03-18 09:01:36 +0100

    Import and use sauces router on app.js

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const authorizeUser = require('./middleware/authorization');
 const errorHandler = require('./middleware/error-handler');
 
 const authRouter = require('./users/auth.routes');
+const saucesRouter = require('./sauces/sauces.routes');
 
 app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', authRouter);
+app.use('/api/sauces', authorizeUser, saucesRouter);
 
 app.use(errorHandler);
 
