24d63c644c861b03f3196b2995aef89767059a61
mbourjac2 <michael.bourjac@gmail.com>
2023-04-21 20:15:02 +0200

    Add file, mongoose and multer error handlers

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const authorizeUser = require('./middleware/authorization');
-const errorHandler = require('./middleware/error-handler');
+const {
+	fileErrorHandler,
+	httpErrorHandler,
+	mongooseErrorHandler,
+	multerErrorHandler,
+} = require('./middleware/error-handlers');
 
 const authRouter = require('./users/auth.routes');
 const saucesRouter = require('./sauces/sauces.routes');
@@ -16,6 +21,11 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', authRouter);
 app.use('/api/sauces', authorizeUser, saucesRouter);
 
-app.use(errorHandler);
+app.use(
+	fileErrorHandler,
+	httpErrorHandler,
+	mongooseErrorHandler,
+	multerErrorHandler
+);
 
 module.exports = app;
---
+++ middleware/error-handlers.js
@@ -0,0 +1,45 @@
+const multer = require('multer');
+const { unlink } = require('fs/promises');
+const { HttpError } = require('../errors');
+
+const fileErrorHandler = async (error, req, res, next) => {
+	if (req.file) {
+		await unlink(req.file.path);
+	}
+
+	next(error);
+};
+
+const httpErrorHandler = async (error, req, res, next) => {
+	if (error instanceof HttpError) {
+		console.error(error);
+		return res.status(error.statusCode).json({ error });
+	}
+
+	next(error);
+};
+
+const mongooseErrorHandler = async (error, req, res, next) => {
+	if (error.name === 'ValidationError' || error.name === 'CastError') {
+		console.error(error);
+		return res.status(400).json({ error });
+	}
+
+	next(error);
+};
+
+const multerErrorHandler = async (error, req, res, next) => {
+	if (error instanceof multer.MulterError) {
+		console.error(error);
+		return res.status(500).json({ error });
+	}
+
+	next(error);
+};
+
+module.exports = {
+	fileErrorHandler,
+	httpErrorHandler,
+	mongooseErrorHandler,
+	multerErrorHandler,
+};
