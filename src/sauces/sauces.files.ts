508ca0573f6aed3d3b43042e6eca93880e09bc4c
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 12:04:50 +0200

    Create MockFileHandler implementation

--- src/sauces/sauces.files.ts
+++ src/sauces/sauces.files.ts
@@ -4,6 +4,27 @@ export interface FileHandler {
   deleteFile(filename: string): Promise<void>;
 }
 
+export class MockFileHandler implements FileHandler {
+  private constructor(private files: string[]) {}
+
+  static init(): MockFileHandler {
+    return new MockFileHandler([]);
+  }
+
+  withFiles(files: string[]): MockFileHandler {
+    this.files = files;
+    return this;
+  }
+
+  getFiles(): string[] {
+    return this.files;
+  }
+
+  async deleteFile(filename: string): Promise<void> {
+    this.files = this.files.filter((file) => file !== filename);
+  }
+}
+
 class FsFileHandler implements FileHandler {
   async deleteFile(filename: string): Promise<void> {
     await unlink(`images/${filename}`);
