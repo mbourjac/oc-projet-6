26c36fec064fb62bbb8d14a9d1efb635b94036b1
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 10:24:18 +0200

    Create BcryptPasswordHasher implementation

--- src/users/users.password.ts
+++ src/users/users.password.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export interface PasswordHasher {
   hashPassword(password: string): Promise<string>;
   comparePassword(
@@ -5,3 +7,19 @@ export interface PasswordHasher {
     password: string
   ): Promise<boolean>;
 }
+
+class BcryptPasswordHasher implements PasswordHasher {
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+
+  async comparePassword(
+    candidatePassword: string,
+    password: string
+  ): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, password);
+  }
+}
+
+export const bcryptPasswordHasher = new BcryptPasswordHasher();
