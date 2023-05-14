604a2249d339e7735530a2d5c632536c38380a8c
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 10:42:20 +0200

    Create JwtTokenHandler implementation

--- src/authentication/authentication.token.ts
+++ src/authentication/authentication.token.ts
@@ -1,4 +1,33 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
 export interface TokenHandler {
   createToken(identifier: string): string;
   getPayload(token: string): unknown;
+}
+
+export class JwtTokenHandler implements TokenHandler {
+  private static instance: JwtTokenHandler;
+  private readonly secretKey: string;
+
+  private constructor(secretKey: string) {
+    this.secretKey = secretKey;
+  }
+
+  public static getInstance(secretKey: string): JwtTokenHandler {
+    if (!JwtTokenHandler.instance) {
+      JwtTokenHandler.instance = new JwtTokenHandler(secretKey);
+    }
+
+    return JwtTokenHandler.instance;
+  }
+
+  createToken(id: string): string {
+    return jwt.sign({ userId: id }, this.secretKey, {
+      expiresIn: '24h',
+    });
+  }
+
+  getPayload(token: string): string | JwtPayload {
+    return jwt.verify(token, this.secretKey);
+  }
 }
