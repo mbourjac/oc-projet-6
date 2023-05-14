6bd21b2fe6067b4f9851f7daa01bc0eaeedaa65f
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 15:50:01 +0200

    Throw error for unprovided database uri case

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import 'dotenv/config';
 import { app } from './app.js';
-import { connectDB } from './database/connect.js';
+import { connectDB } from './database/database.connect.js';
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    const databaseURI = process.env.MONGO_URI;
+
+    if (!databaseURI) {
+      throw new Error('Please provide a database URI');
+    }
+
+    await connectDB(databaseURI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
