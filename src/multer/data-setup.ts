cd7a23e362b70686773aef34858febc057e13594
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 11:29:19 +0200

    Add types to dataSetup middleware

--- src/multer/data-setup.ts
+++ src/multer/data-setup.ts
@@ -1,11 +1,17 @@
-const dataSetup = (req, res, next) => {
-  if (req.file) {
-    const sauce = JSON.parse(req.body.sauce);
+import { RequestHandler } from 'express';
+import { ISetupSauceData, IValidateSauce } from '../sauces/sauces.types.js';
+import { ITypeRequestBody } from '../request/request.types.js';
+
+export const dataSetup: RequestHandler = (
+  req: ITypeRequestBody<ISetupSauceData>,
+  res,
+  next
+): void => {
+  if (req.file && req.body.sauce) {
+    const sauce: IValidateSauce = JSON.parse(req.body.sauce);
 
     Object.assign(req.body, sauce);
   }
 
   next();
 };
-
-export { dataSetup };
