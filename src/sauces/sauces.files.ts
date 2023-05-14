9f7eacd946ecbe80593eada196356d689f991a78
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 12:04:12 +0200

    Create FsFileHandler implementation

--- src/sauces/sauces.files.ts
+++ src/sauces/sauces.files.ts
@@ -1,3 +1,13 @@
+import { unlink } from 'fs/promises';
+
 export interface FileHandler {
   deleteFile(filename: string): Promise<void>;
 }
+
+class FsFileHandler implements FileHandler {
+  async deleteFile(filename: string): Promise<void> {
+    await unlink(`images/${filename}`);
+  }
+}
+
+export const fsFileHandler = new FsFileHandler();
