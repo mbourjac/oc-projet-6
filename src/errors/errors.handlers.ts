aff0116a015c0fbd0a24c439f2c3f426e89e724d
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 09:49:26 +0200

    Rename and move error handlers to errors folder

---
+++ src/errors/errors.handlers.ts
@@ -0,0 +1,62 @@
+import multer from 'multer';
+import { ErrorRequestHandler } from 'express';
+import { unlink } from 'fs/promises';
+import { HttpError } from './errors.http-error.js';
+
+export const fileErrorHandler: ErrorRequestHandler = async (
+  error,
+  req,
+  res,
+  next
+): Promise<void> => {
+  if (req.file) {
+    await unlink(req.file.path);
+  }
+
+  next(error);
+};
+
+export const httpErrorHandler: ErrorRequestHandler = async (
+  error,
+  req,
+  res,
+  next
+): Promise<void> => {
+  if (error instanceof HttpError) {
+    console.error(error);
+    res.status(error.statusCode).json({ error });
+    return;
+  }
+
+  next(error);
+};
+
+export const mongooseErrorHandler: ErrorRequestHandler = async (
+  error,
+  req,
+  res,
+  next
+): Promise<void> => {
+  if (error.name === 'ValidationError' || error.name === 'CastError') {
+    console.error(error);
+    res.status(400).json({ error });
+    return;
+  }
+
+  next(error);
+};
+
+export const multerErrorHandler: ErrorRequestHandler = async (
+  error,
+  req,
+  res,
+  next
+): Promise<void> => {
+  if (error instanceof multer.MulterError) {
+    console.error(error);
+    res.status(500).json({ error });
+    return;
+  }
+
+  next(error);
+};
