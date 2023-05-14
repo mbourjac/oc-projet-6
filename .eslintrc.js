1dee987b447ddf2ef858400e751c43f8b5b71aa2
mbourjac2 <michael.bourjac@gmail.com>
2023-05-14 09:21:35 +0200

    Turn eslint rule prefer-default-export off

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    'import/prefer-default-export': 'off',
     'no-alert': 'off',
     'no-console': 'off',
     'default-case': 'off',
