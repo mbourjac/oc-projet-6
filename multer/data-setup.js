815e6e025cf7981981e46100f8a17e89064908d5
mbourjac2 <michael.bourjac@gmail.com>
2023-05-21 16:45:43 +0200

    Replace Object.assign with spread syntax

--- multer/data-setup.js
+++ multer/data-setup.js
@@ -2,7 +2,7 @@ const dataSetup = (req, res, next) => {
 	if (req.file) {
 		const sauce = JSON.parse(req.body.sauce);
 
-		Object.assign(req.body, sauce);
+		req.body = {...req.body, ...sauce}
 	}
 
 	next();
