a6c87f3d8322eba41be98274773926d037588f3f
mbourjac2 <michael.bourjac@gmail.com>
2023-05-21 16:29:24 +0200

    Fix return statement of validationCheck middleware

--- middleware/validation-check.js
+++ middleware/validation-check.js
@@ -4,7 +4,8 @@ const validationCheck = (req, res, next) => {
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
-		return res.status(400).json({ errors: errors.array() });
+		res.status(400).json({ errors: errors.array() });
+    return;
 	}
 
 	next();
